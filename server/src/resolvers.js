f1c644ca935151d947d931aaf83e9631b03531f4
Safnaj <safnaj99@gmail.com>
2022-12-26 13:18:37 +0530

    Graph Server

---
+++ server/src/resolvers.js
@@ -0,0 +1,17 @@
+import { Company, Job } from "./db.js";
+
+export const resolvers = {
+  Query: {
+    company: (_root, { id }) => Company.findById(id),
+    job: (_root, { id }) => Job.findById(id),
+    jobs: async () => await Job.findAll(),
+  },
+
+  Company: {
+    jobs: (company) => Job.findAll((job) => job.companyId === company.id),
+  },
+
+  Job: {
+    company: (job) => Company.findById(job.companyId),
+  },
+};
