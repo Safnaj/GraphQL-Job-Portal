c0b2c24bc236c9a657c503278bbe2e4b97a707c8
Safnaj <safnaj99@gmail.com>
2022-12-29 12:36:18 +0530

    Schema Input Types

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -7,6 +7,12 @@ export const resolvers = {
     jobs: async () => await Job.findAll(),
   },
 
+  Mutation: {
+    createJob: async (_root, { input }) => {
+      return Job.create(input);
+    },
+  },
+
   Company: {
     jobs: (company) => Job.findAll((job) => job.companyId === company.id),
   },
@@ -15,3 +21,12 @@ export const resolvers = {
     company: (job) => Company.findById(job.companyId),
   },
 };
+
+/*
+  - We can wite reolvers for any fields we want, if we do provdie a reolver function, thats what the GraphQL framework will
+    call to get the value for the field.
+  - For every Job object it will call Company resovler since it has a company type.
+  - ID, Title & Description are scalar types which can be return from Jobs in Query. 
+  - We need to write a resolver function for a custom type like Company only if we need some special logic to find value for 
+    the field.
+*/
